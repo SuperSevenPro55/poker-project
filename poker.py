542b78ba3be99d11032b08bcde98038c7932e6bc
SuperSevenPro55 <95611814+SuperSevenPro55@users.noreply.github.com>
2021-12-06 17:08:07 +0300

    Very raw

---
+++ poker.py
@@ -0,0 +1,11 @@
+def game_begining(gamers, score, a):
+    gamers=int(input('Введите количество игроков: ')) # определяем игроков
+    score=int(input('Введите количество фишек у игроков: '))# раздаем фишки
+    a=list() #вспомогательный список
+    for i in range(gamers):
+        a.insert(999999999, score)# костыли для раздачи фишек игрокам
+    for i in range(gamers):
+        a_i=a[i] # разделяем список на переменные
+        
+def dealer_choose(dealer):
+    dealer=input('Выберите дилера(номер игрока): ') # определяем дилера 
